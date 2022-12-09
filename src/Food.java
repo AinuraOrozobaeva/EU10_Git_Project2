122fba44447d2ccbb2a15cd4669812c6c667a541
44757 <asinoori555@gmail.com>
2022-12-09 19:55:13 +0000

    update added to Food

--- src/Food.java
+++ src/Food.java
@@ -5,5 +5,6 @@ public class Food {
         System.out.println("Food is essential for living");
         System.out.println("We need proper food for coding lol");
         System.out.println("We are working on project called Food");
+        System.out.println("args = " + args);
     }
 }
