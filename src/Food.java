1c6b7c8fb7c4f7d1179b2f6099257b2a77919699
44757 <asinoori555@gmail.com>
2022-12-09 13:47:39 +0000

    Created class 'Food'

---
+++ src/Food.java
@@ -0,0 +1,8 @@
+public class Food {
+
+    public static void main(String[] args) {
+
+        System.out.println("Food is essential for living");
+        System.out.println("We need proper food for coding lol");
+    }
+}
